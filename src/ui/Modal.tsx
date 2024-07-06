a735d76f717d1056c22d7aec0f4e599e60185958
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-07-06 22:03:53 +0400

    modal edit

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -6,24 +6,24 @@ import {
   useContext,
   useEffect,
   useState,
-} from 'react';
-import { createPortal } from 'react-dom';
-import { IoIosClose } from 'react-icons/io';
+} from "react";
+import { createPortal } from "react-dom";
+import { IoIosClose } from "react-icons/io";
 
-import { ModalContextType } from '../types/contextTypes';
-import Button from './Button';
-import { useOutsideClick } from '../hooks/useOutsideClick';
+import { ModalContextType } from "../types/contextTypes";
+import Button from "./Button";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const ModalContext = createContext<ModalContextType>(null);
 
 const Modal = ({ children }: { children: ReactNode }) => {
-  const [openWindowName, setOpenWindowName] = useState<string>('');
+  const [openWindowName, setOpenWindowName] = useState<string>("");
   const openWindow = setOpenWindowName;
-  const closeWindow = () => setOpenWindowName('');
+  const closeWindow = () => setOpenWindowName("");
 
   useEffect(() => {
-    if (openWindowName) document.body.style.overflow = 'hidden';
-    else document.body.style.overflow = 'auto';
+    if (openWindowName) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "auto";
   }, [openWindowName]);
 
   return (
@@ -43,7 +43,7 @@ const useModalContext = () => {
   const context = useContext(ModalContext);
 
   if (!context)
-    throw new Error('Component cannot be used outside the provider');
+    throw new Error("Component cannot be used outside the provider");
 
   return context;
 };
@@ -67,15 +67,15 @@ const Window = ({ children, name }: { children: ReactNode; name: string }) => {
   if (openWindowName !== name) return null;
 
   return createPortal(
-    <div className='fixed inset-0 w-full h-screen bg-black/60 transition duration-500 z-[1000] animate-fadeIn opacity-0 scale-110 backdrop-blur-sm'>
+    <div className="fixed inset-0 w-full h-screen bg-black/60 transition duration-500 z-[1000] animate-fadeIn opacity-0 scale-110">
       <div
         ref={ref}
-        className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md px-12 py-8 bg-white text-neutral-gray-dark'
+        className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md px-12 py-8 bg-white text-neutral-gray-dark"
       >
-        <div className='flex flex-col gap-6'>{children}</div>
+        <div className="flex flex-col gap-6">{children}</div>
       </div>
     </div>,
-    document.body
+    document.body,
   );
 };
 
@@ -93,9 +93,9 @@ const Head = ({ children }: { children: ReactNode }) => {
   const { closeWindow } = useModalContext();
 
   return (
-    <div className='flex justify-between'>
+    <div className="flex justify-between">
       <h5>{children}</h5>
-      <Button size='icon' onClick={closeWindow}>
+      <Button size="icon" onClick={closeWindow}>
         <IoIosClose />
       </Button>
     </div>
